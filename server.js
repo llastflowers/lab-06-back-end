3158f482caa03eacb96a2f02ceb620181230b85e
Ben Beekman <bbeekman@gmail.com>
2019-11-06 10:18:56 -0800

    fixed location endpoint

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const superagent = require('superagent');
 
 const app = express();
 const PORT = process.env.PORT;
 app.use(cors());
 
-const geoData = require('./data/geo.json');
 const weatherData = require('./data/darksky.json');
 
 function toWeather(weather) {
@@ -32,13 +32,13 @@ function toLocation(geo) {
     };
 }
 
-function getLatLng(location) {
-    if (location === 'bad location') {
-        throw new Error();
-    }
+// function getLatLng(location) {
+//     if (location === 'bad location') {
+//         throw new Error();
+//     }
 
-    return toLocation(geoData);
-}
+//     return toLocation(geoData);
+// }
 
 function getWeatherLoc(location) {
     if (location === 'bad location') {
@@ -48,10 +48,14 @@ function getWeatherLoc(location) {
     return toWeather(weatherData);
 }
 
-app.get('/location', (request, response) => {
+app.get('/location', async(request, response) => {
     try {
-        const location = request.query.location;
-        const result = getLatLng(location);
+        const location = request.query.search;
+        const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
+        const mapsLocation = await superagent.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${GEOCODE_API_KEY}`);
+        const parsedMapLocation = JSON.parse(mapsLocation.text);
+        console.log(parsedMapLocation);
+        const result = toLocation(parsedMapLocation);
         response.status(200).json(result);
     } catch (err) {
         response.status(500).send('Sorry, something went wrong, please try again!');
